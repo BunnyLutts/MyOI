702bca25978aa01bb70cb9cc288601c8c3c42714
Lutts <lutts@qq.com>
2018-12-22 10:12:07 +0800

    完成 jzoj contest 2565 0 对拍,jzoj contest 2565 0 需判重

--- jzoj/contest/2565/0.cpp
+++ jzoj/contest/2565/0.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+long long calc(long long x) {
+  if (x<1) {
+    return 0;
+  }
+  long long ans=0;
+  for (int i=0; i<9; i++) {
+    int temp = x/(i+1)-1;
+    if (temp>=i) {
+      ans += (temp-i)/9+1;
+    }
+  }
+  return ans;
+}
+
 int main() {
 #ifdef DEBUG
   freopen("0.in", "r", stdin);
@@ -12,19 +26,9 @@ int main() {
   int t;
   scanf("%d", &t);
   for (int i=1; i<=t; i++) {
-    long long l, r, ans=0;
+    long long l, r;
     scanf("%lld %lld", &l, &r);
-    for (long long j=l; j<=r; j++) {
-      bool flag=false;
-      for (int k=1; k<10; k++) {
-	if (j%k==0 && (j/k-1)%9+1==k) {
-	  flag = true;
-	  break;
-	}
-      }
-      ans += flag;
-    }
-    printf("%lld\n", ans);
+    printf("%lld\n", calc(r)-calc(l-1));
   }
 
   fclose(stdin);
