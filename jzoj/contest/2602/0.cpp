0045cbe91e138b3c8d75f06883f90226fa1927f9
Lutts <lutts@qq.com>
2019-01-23 14:30:34 +0800

    AC JZOJ Contest2602 0

--- jzoj/contest/2602/0.cpp
+++ jzoj/contest/2602/0.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-long long min(long long a, long long b) {
+unsigned long long min(unsigned long long a, unsigned long long b) {
   return a<b ? a : b;
 }
 
-long long max(long long a, long long b) {
+unsigned long long max(unsigned long long a, unsigned long long b) {
   return a>b ? a : b;
 }
 
@@ -17,22 +17,26 @@ int main() {
   freopen("0.out", "w", stdout);
 #endif
 
-  int k, a, b;
-  scanf("%d %d %d", &k, &a, &b);
-  long long ans=0;
+  unsigned long long k, a, b;
+  scanf("%llu %llu %llu", &k, &a, &b);
+  unsigned long long ans=0;
+  if (k==0) {
+    printf("%llu", b-a+1);
+    return 0;
+  }
   if (k%2) {
     if (a<=k && k<=b) {
       ans++;
     }
     k*=2;
   }
-  for (long long x=1; x*k<=b; x*=2) {
-    long long l=x*k, r=x*k+2*x-1;
+  for (unsigned long long x=1; x*k<=b; x*=2L) {
+    unsigned long long l=x*k, r=x*k+2*x-1L;
     if (l<=b && r>=a) {
-      ans += min(b, r)-max(a, l)+1;
+      ans += min(b, r)-max(a, l)+1L;
     }
   }
-  printf("%lld", ans);
+  printf("%llu", ans);
 
   fcloseall();
   return 0;
